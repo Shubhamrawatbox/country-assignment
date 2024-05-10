c4295fb97ea99c640323fde516f9cd1711ee7dea
Shubham <shubham.rawat@turningcloud.com>
2024-05-10 21:33:44 +0530

    some changes

--- src/hooks/getCountryCurrency.ts
+++ src/hooks/getCountryCurrency.ts
@@ -2,7 +2,6 @@
 
 export const getCountryCurrency = (currencyValue: any) => {
   const keys = Object.keys(currencyValue);
-
   const currencyName = currencyValue[keys[0]].name;
-  return currencyName?.name
+  return currencyName
 };
